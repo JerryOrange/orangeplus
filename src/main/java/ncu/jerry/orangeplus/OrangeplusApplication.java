86c960b805f6d5721be1457110c95a948763400b
JerryOrange <563146556@qq.com>
2017-08-10 17:19:11 +0800

    add ErrorPage

--- src/main/java/ncu/jerry/orangeplus/OrangeplusApplication.java
+++ src/main/java/ncu/jerry/orangeplus/OrangeplusApplication.java
@@ -2,6 +2,10 @@ package ncu.jerry.orangeplus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 public class OrangeplusApplication {
@@ -9,4 +13,16 @@ public class OrangeplusApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrangeplusApplication.class, args);
 	}
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+        return (container -> {
+            ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+            ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+
+            container.addErrorPages(error401Page, error404Page, error500Page);
+        });
+    }
 }
