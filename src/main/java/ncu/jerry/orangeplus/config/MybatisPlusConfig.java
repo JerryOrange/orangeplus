95bb643b1871d0aede2a8642930f42d17f783486
JerryOrange <563146556@qq.com>
2017-08-14 16:57:12 +0800

    add mybatis-plus分页插件

---
+++ src/main/java/ncu/jerry/orangeplus/config/MybatisPlusConfig.java
@@ -0,0 +1,20 @@
+package ncu.jerry.orangeplus.config;
+
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("ncu.jerry.orangeplus.modules.*.mapper*")
+public class MybatisPlusConfig {
+
+    /**
+     * mybatis-plus分页插件<br>
+     * 文档：http://mp.baomidou.com<br>
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
