8c1f2e3e6bdc2f6a93ef852199fc3ee7e52a4f06
JerryOrange <563146556@qq.com>
2017-08-15 16:45:01 +0800

    新增部门Mapper接口

---
+++ src/main/java/ncu/jerry/orangeplus/modules/sys/mapper/DeptMapper.java
@@ -0,0 +1,16 @@
+package ncu.jerry.orangeplus.modules.sys.mapper;
+
+import ncu.jerry.orangeplus.modules.sys.entity.Dept;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+  * 部门表 Mapper 接口
+ * </p>
+ *
+ * @author Jiacheng
+ * @since 2017-08-11
+ */
+public interface DeptMapper extends BaseMapper<Dept> {
+
+}
