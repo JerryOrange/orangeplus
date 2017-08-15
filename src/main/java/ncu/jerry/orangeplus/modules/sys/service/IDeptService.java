57296819ad4f136b378047801b8db4ec6855d2f4
JerryOrange <563146556@qq.com>
2017-08-15 16:45:44 +0800

    新增部门模块service层

---
+++ src/main/java/ncu/jerry/orangeplus/modules/sys/service/IDeptService.java
@@ -0,0 +1,16 @@
+package ncu.jerry.orangeplus.modules.sys.service;
+
+import ncu.jerry.orangeplus.modules.sys.entity.Dept;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ * 部门表 服务类
+ * </p>
+ *
+ * @author Jiacheng
+ * @since 2017-08-11
+ */
+public interface IDeptService extends IService<Dept> {
+	
+}
---
+++ src/main/java/ncu/jerry/orangeplus/modules/sys/service/impl/DeptServiceImpl.java
@@ -0,0 +1,20 @@
+package ncu.jerry.orangeplus.modules.sys.service.impl;
+
+import ncu.jerry.orangeplus.modules.sys.entity.Dept;
+import ncu.jerry.orangeplus.modules.sys.mapper.DeptMapper;
+import ncu.jerry.orangeplus.modules.sys.service.IDeptService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 部门表 服务实现类
+ * </p>
+ *
+ * @author Jiacheng
+ * @since 2017-08-11
+ */
+@Service
+public class DeptServiceImpl extends ServiceImpl<DeptMapper, Dept> implements IDeptService {
+	
+}
