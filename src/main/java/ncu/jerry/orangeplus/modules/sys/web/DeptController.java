ffa89cc29a8271e63295b6e1fcbb9480fae04916
JerryOrange <563146556@qq.com>
2017-08-15 16:48:24 +0800

    新增部门Controller

---
+++ src/main/java/ncu/jerry/orangeplus/modules/sys/web/DeptController.java
@@ -0,0 +1,20 @@
+package ncu.jerry.orangeplus.modules.sys.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * 部门 前端控制器
+ * </p>
+ *
+ * @author Jiacheng
+ * @since 2017-08-11
+ */
+@Controller
+@RequestMapping("/dept")
+public class DeptController {
+	
+}
