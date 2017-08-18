2bf9188736af9862e01739a0714cbd4b753a32e6
JerryOrange <563146556@qq.com>
2017-08-18 12:34:36 +0800

    新增角色role

---
+++ src/main/java/ncu/jerry/orangeplus/modules/sys/mapper/RoleMapper.java
@@ -0,0 +1,16 @@
+package ncu.jerry.orangeplus.modules.sys.mapper;
+
+import ncu.jerry.orangeplus.modules.sys.entity.Role;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+  * 角色表 Mapper 接口
+ * </p>
+ *
+ * @author Jiacheng
+ * @since 2017-08-11
+ */
+public interface RoleMapper extends BaseMapper<Role> {
+
+}
---
+++ src/main/java/ncu/jerry/orangeplus/modules/sys/service/IRoleService.java
@@ -0,0 +1,16 @@
+package ncu.jerry.orangeplus.modules.sys.service;
+
+import ncu.jerry.orangeplus.modules.sys.entity.Role;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ * 角色表 服务类
+ * </p>
+ *
+ * @author Jiacheng
+ * @since 2017-08-11
+ */
+public interface IRoleService extends IService<Role> {
+	
+}
