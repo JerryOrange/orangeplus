c062dc11ad5ba12cafb45f710e755b30ab68840c
JerryOrange <563146556@qq.com>
2017-08-26 17:01:20 +0800

    新增系统字典

---
+++ src/main/java/ncu/jerry/orangeplus/modules/sys/service/IDictService.java
@@ -0,0 +1,16 @@
+package ncu.jerry.orangeplus.modules.sys.service;
+
+import ncu.jerry.orangeplus.modules.sys.entity.Dict;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ * 字典表 服务类
+ * </p>
+ *
+ * @author Jiacheng
+ * @since 2017-08-23
+ */
+public interface IDictService extends IService<Dict> {
+	
+}
