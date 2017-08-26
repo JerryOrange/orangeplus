ed9957d5acf91b6bbd61f66666c537ec36bac671
JerryOrange <563146556@qq.com>
2017-08-26 16:59:25 +0800

    新增登陆日志

---
+++ src/main/java/ncu/jerry/orangeplus/modules/sys/mapper/LoginLogMapper.java
@@ -0,0 +1,16 @@
+package ncu.jerry.orangeplus.modules.sys.mapper;
+
+import ncu.jerry.orangeplus.modules.sys.entity.LoginLog;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+  * 登录记录 Mapper 接口
+ * </p>
+ *
+ * @author Jiacheng
+ * @since 2017-08-23
+ */
+public interface LoginLogMapper extends BaseMapper<LoginLog> {
+
+}
