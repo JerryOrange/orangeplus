5f167296f1e05d87d9a78cd9b439b6c38c2b29c6
JerryOrange <563146556@qq.com>
2017-12-20 10:14:29 +0800

    新增生产者2

---
+++ src/main/java/ncu/jerry/orangeplus/rabbitMq/P2P/HelloSender2.java
@@ -0,0 +1,25 @@
+package ncu.jerry.orangeplus.rabbitMq.P2P;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * Created by Jiacheng on 2017/12/20.
+ * 生产者2
+ */
+@Component
+public class HelloSender2 {
+
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    public void send(String msg) {
+        String sendMsg = msg + new Date();
+        System.out.println("Sender2 : " + sendMsg);
+        this.rabbitTemplate.convertAndSend("helloQueue", sendMsg);
+    }
+
+}
