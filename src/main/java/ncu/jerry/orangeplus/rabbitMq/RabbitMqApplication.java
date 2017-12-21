42cd9b5fbf5190b823bf073ae6a282e72831bdc1
JerryOrange <563146556@qq.com>
2017-12-21 14:22:29 +0800

    创建实体类传输队列

--- src/main/java/ncu/jerry/orangeplus/rabbitMq/RabbitMqApplication.java
+++ src/main/java/ncu/jerry/orangeplus/rabbitMq/RabbitMqApplication.java
@@ -2,19 +2,27 @@ package ncu.jerry.orangeplus.rabbitMq;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Jiacheng on 2017/12/18.
  * <p>
  * 创建初始化队列
  */
+@Component
 public class RabbitMqApplication {
 
-    final static String queueName = "hello";
+    final static String queueName = "helloQueue";
+    final static String userQueueName = "userQueue";
 
     @Bean
     public Queue helloQueue() {
         return new Queue(queueName);
+    }
+
+    @Bean
+    public Queue userQueue() {
+        return new Queue(userQueueName);
     }
 
 }
