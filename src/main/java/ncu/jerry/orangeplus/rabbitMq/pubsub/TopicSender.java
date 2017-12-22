20f54520cc9f32b9842a826418e18487eba3c949
JerryOrange <563146556@qq.com>
2017-12-22 11:30:26 +0800

    生产者：向exchange发送不同队列消息

---
+++ src/main/java/ncu/jerry/orangeplus/rabbitMq/pubsub/TopicSender.java
@@ -0,0 +1,34 @@
+package ncu.jerry.orangeplus.rabbitMq.pubsub;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Jiacheng on 2017/12/22.
+ * topic Exchange 生产者
+ */
+@Component
+public class TopicSender {
+
+    protected final static Logger logger = LoggerFactory.getLogger(TopicSender.class);
+
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    public void send() {
+        String msg1 = "topic.mesaage msg1";
+        logger.info("sender1 : " + msg1);
+        this.rabbitTemplate.convertAndSend("exchange", "topic.message", msg1);
+
+        String msg2 = "topic.mesaages msg2";
+        logger.info("sender2 : " + msg2);
+        this.rabbitTemplate.convertAndSend("exchange", "topic.messages", msg2);
+
+        String msg3 = "topic.mesaages msg3";
+        logger.info("sender3 : " + msg3);
+        this.rabbitTemplate.convertAndSend("exchange2", "topic.message", msg3);
+    }
+}
