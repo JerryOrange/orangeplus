a8e28ef18ceaf28799cae01971bcbaeb61ae604e
JerryOrange <563146556@qq.com>
2017-12-22 11:26:50 +0800

    Initializing exchange, binding exchange with routingKey

--- src/main/java/ncu/jerry/orangeplus/rabbitMq/RabbitMqApplication.java
+++ src/main/java/ncu/jerry/orangeplus/rabbitMq/RabbitMqApplication.java
@@ -1,6 +1,9 @@
 package ncu.jerry.orangeplus.rabbitMq;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +11,7 @@ import org.springframework.stereotype.Component;
  * Created by Jiacheng on 2017/12/18.
  * <p>
  * 创建初始化队列
+ * Initializing exchange, binding exchange with routingKey
  */
 @Component
 public class RabbitMqApplication {
@@ -23,6 +27,60 @@ public class RabbitMqApplication {
     @Bean
     public Queue userQueue() {
         return new Queue(userQueueName);
+    }
+
+    //topic Exchange Queue
+    @Bean
+    public Queue queueMessage() {
+        return new Queue("topic.message");
+    }
+
+    @Bean
+    public Queue queueMessages() {
+        return new Queue("topic.messages");
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("exchange");
+    }
+
+    @Bean
+    TopicExchange exchange2() {
+        return new TopicExchange("exchange2");
+    }
+
+    /**
+     * 将队列topic.message与exchange绑定，binding_key为topic.message,完全匹配
+     * @param queueMessage
+     * @param exchange
+     * @return
+     */
+    @Bean
+    Binding bindingExchangeMessage(Queue queueMessage, TopicExchange exchange) {
+        return BindingBuilder.bind(queueMessage).to(exchange).with("topic.message");
+    }
+
+    /**
+     * 将队列topic.messages与exchange绑定，binding_key为topic.#,模糊匹配
+     * @param queueMessages
+     * @param exchange
+     * @return
+     */
+    @Bean
+    Binding bindingExchangeMessages(Queue queueMessages, TopicExchange exchange) {
+        return BindingBuilder.bind(queueMessages).to(exchange).with("topic.#");
+    }
+
+    /**
+     * 将队列topic.message与exchange2绑定，binding_key为topic.message,完全匹配
+     * @param queueMessage
+     * @param exchange2
+     * @return
+     */
+    @Bean
+    Binding bindingExchange2Message(Queue queueMessage, TopicExchange exchange2) {
+        return BindingBuilder.bind(queueMessage).to(exchange2).with("topic.message");
     }
 
 }
