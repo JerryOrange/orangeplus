3eec374b6f8e44c75951f99db452d98626dbcc54
JerryOrange <563146556@qq.com>
2018-01-17 14:24:04 +0800

    bug fixes:调整ErrorPage静态path问题

--- src/main/java/ncu/jerry/orangeplus/OrangeplusApplication.java
+++ src/main/java/ncu/jerry/orangeplus/OrangeplusApplication.java
@@ -1,17 +1,13 @@
 package ncu.jerry.orangeplus;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
@@ -27,9 +23,9 @@ public class OrangeplusApplication extends WebMvcConfigurerAdapter {
     public EmbeddedServletContainerCustomizer containerCustomizer() {
 
         return (container -> {
-            ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
-            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
-            ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+            ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/static/errorPage/401.html");
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/static/errorPage/404.html");
+            ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/static/errorPage/500.html");
 
             container.addErrorPages(error401Page, error404Page, error500Page);
         });
