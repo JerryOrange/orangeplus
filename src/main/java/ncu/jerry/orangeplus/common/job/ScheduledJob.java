18a7e0aad5c127f1bf1bbca864e689f112d0e39e
JerryOrange <563146556@qq.com>
2018-01-27 14:34:35 +0800

    新增Spring @Scheduled方式实现定时任务示例

---
+++ src/main/java/ncu/jerry/orangeplus/common/job/ScheduledJob.java
@@ -0,0 +1,61 @@
+package ncu.jerry.orangeplus.common.job;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * Spring @Scheduled 实现定时任务
+ *
+ * Created by Jiacheng on 2018/1/27.
+ */
+@Component
+@EnableScheduling
+public class ScheduledJob {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ScheduledJob.class);
+
+    /**
+     * 每分钟执行一次
+     *
+     * @throws Exception
+     */
+    @Scheduled(cron = "0 0/1 * * * ?")
+    public void work() throws Exception {
+        LOGGER.debug("执行调度任务：" + new Date());
+    }
+
+    /**
+     * 每5秒执行一次
+     *
+     * @throws Exception
+     */
+    @Scheduled(fixedRate = 5000)
+    public void play() throws Exception {
+        LOGGER.debug("执行定时器任务：" + new Date());
+    }
+
+    /**
+     * 每2秒执行一次
+     *
+     * @throws Exception
+     */
+    @Scheduled(cron = "0/2 * * * * ?")
+    public void doSomething() throws Exception {
+        LOGGER.debug("每2秒执行一个的定时任务：" + new Date());
+    }
+
+    /**
+     * 每一小时执行一次
+     *
+     * @throws Exception
+     */
+    @Scheduled(cron = "0 0 0/1 * * ? ")
+    public void goWork() throws Exception {
+        LOGGER.debug("每一小时执行一次的定时任务：" + new Date());
+    }
+}
