35e2ba267097ce63cbd197e78c8a1d87b0962a08
JerryOrange <563146556@qq.com>
2018-01-31 14:47:27 +0800

    Quartz Cron配置实体类

---
+++ src/main/java/ncu/jerry/orangeplus/common/job/QuartzCronConfig.java
@@ -0,0 +1,35 @@
+package ncu.jerry.orangeplus.common.job;
+
+import javax.persistence.*;
+
+/**
+ * Created by Jiacheng on 2018/1/29.
+ */
+@Entity
+public class QuartzCronConfig {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String cron;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCron() {
+        return cron;
+    }
+
+    public void setCron(String cron) {
+        this.cron = cron;
+    }
+}
